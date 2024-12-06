ab8fd7c818ccdbd9b0f4246ce6e9ae34d7d4c7e5
banskotanup <baskota.anup11@gmail.com>
2024-12-06 12:42:34 +0545

    Add new toggle feature

--- PyramidGenerator/script.js
+++ PyramidGenerator/script.js
@@ -1,16 +1,22 @@
 const character = "#";
 const count = 8;
 const rows = [];
+const inverted = false;
 
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 for (let i = 1; i <= count; i++){
-    rows.push(padRow(i, count));
+    if (inverted) {
+        rows.unshift(padRow(i, count));
+    }
+    else {
+        rows.push(padRow(i, count));
+    }
 }
 
-let result = " ";
+let result = "";
 for (const row of rows) {
     result = result + row + "\n";
 }
